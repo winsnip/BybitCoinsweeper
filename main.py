888d8d03b98ae99f4b8b00aeb8463314bb7fae9f
PandaEver <justpandaever@gmail.com>
2024-09-27 05:37:09 +0700

    Update main.py
    not finished

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os, json, time, requests, crayons, sys
+import os, json, time, requests, crayons, sys, re, hmac, hashlib, random, math
 from datetime import datetime
 import urllib.parse
+
+
+def generate_hash(key, message):
+    hmac_obj = hmac.new(key.encode(), message.encode(), hashlib.sha256)
+    return hmac_obj.hexdigest()
 
 def url_decode(encoded_url):
     return urllib.parse.unquote(encoded_url)
@@ -77,22 +82,37 @@ class ByBit:
     def score(self):
         for i in range(3):
             try:
-                gametime = int(time.time()) % 211 + 90
-                score = int(time.time()) % 301 + 600
+                min_game_time = 60
+                max_game_time = 180
+                game_time = random.randint(min_game_time, max_game_time)
+                min_score = 100
+                max_score = 500
+                score = round(random.uniform(min_score, max_score), 5)
+                starttime = int(time.time() * 1000)
                 playgame = self.session.post("https://api.bybitcoinsweeper.com/api/games/start", json={}, headers=self.headers).json()
+                if "message" in playgame:
+                    if("expired" in playgame["message"]):
+                        self.log("Query Expired Sir", "ERROR")
+                        sys.exit(0)
                 gameid = playgame["id"]
                 rewarddata = playgame["rewards"]
-                self.log(f"Starting game {i + 1}/3. Play time: {gametime} seconds", 'INFO')
-                self.wait(gametime)
+                self.log(f"Starting game {i + 1}/3. Play time: {game_time} seconds", 'INFO')
+                self.wait(game_time)
+                first = f"66f259c3bc25ac58ea3605fcv$2f1-{gameid}-{starttime}"
+                last = f"{game_time}-{gameid}"
+                #66f56e0c335b39ab5102f60av$2f1
+                #66f259c3bc25ac58ea3605fcv$2f1
                 game_data = {
                     "bagCoins": rewarddata["bagCoins"],
                     "bits": rewarddata["bits"],
                     "gifts": rewarddata["gifts"],
                     "gameId": gameid,
-                    'gameTime': gametime,
+                    'gameTime': game_time,
+                    "h": generate_hash(first ,last),
                     'score': score
                 }
                 res = self.session.post('https://api.bybitcoinsweeper.com/api/games/win', json=game_data, headers=self.headers)
+                print(res.text)
                 if res.status_code == 201:
                     self.info["score"] += score
                     self.log(f"Game Played Successfully: received {score} points | Total: {self.info['score']}","SUCCESS")
@@ -101,7 +121,6 @@ class ByBit:
                     return False
                 else:
                     self.log(f"An Error Occurred With Code {res.status_code}", 'ERROR')
-
                 self.wait(5)
             except requests.RequestException:
                 self.log('Too Many Requests, Please Wait', 'WARNING')
@@ -124,6 +143,10 @@ class ByBit:
                 decoded = url_decode(init_data)
                 finaldat = (url_decode(decoded))
                 user_data = json.loads(finaldat.split('user=')[1].split('&')[0])
+                pattern = r'hash=([a-f0-9]{64})'
+                match = re.search(pattern, finaldat)
+                if match:
+                    hash_value = match.group(1)   
                 self.log(f"========== Account {i + 1} | {user_data['first_name']} ==========", 'INFO')
                 self.log(f"self.logging into account {user_data['id']}...", 'INFO')
                 login_result = self.login(init_data)
